111dcbf23139e1d332d90effc745cbccdb522a5b
Vardges Hakobyan <vardgeshakobiann@gmail.com>
2025-03-15 21:51:27 +0400

    second commit

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+const container = document.querySelector('.container');
 const chatsContainer = document.querySelector('.chats-container');
 const promptForm = document.querySelector('.prompt-form');
 const promptInput = promptForm.querySelector('.prompt-input');
+const fileInput = promptForm.querySelector('#file-input');
+const fileUploadWrapper = promptForm.querySelector('.file-upload-wrapper');
 
 const API_KEY = "AIzaSyA8Gb7Z5at5d9ygXCOquy5YssBv8j2YzOA";
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`
 
-let userMessage = "";
 const chatHistory = [];
+let userData = { message: "", file: {} };
 
 const createMsgElement = (content, ...classes) => {
     const div = document.createElement('div');
@@ -14,6 +17,8 @@ const createMsgElement = (content, ...classes) => {
     div.innerHTML = content;
     return div;
 }
+
+const scrollToBottom = () => container.scrollTo({ top: container.scrollHeight, behavior: "smooth" });
 
 const typingEffect = (text, textElement, botMsgDiv) => {
     textElement.textContent = "";
@@ -24,6 +29,7 @@ const typingEffect = (text, textElement, botMsgDiv) => {
         if (wordIndex < words.length) {
             textElement.textContent += (wordIndex === 0 ? "" : " ") + words[wordIndex++];
             botMsgDiv.classList.remove("loading");
+            scrollToBottom();
         } else {
             clearInterval(typingInterval);
         }
@@ -35,7 +41,7 @@ const generateResponse = async (botMsgDiv) => {
 
     chatHistory.push({
         role: "user",
-        parts: [{ text: userMessage }]
+        parts: [{ text: userData.message }, ...(userData.file.data ? [{ inline_data: (({ fileName, isImage, ...rest }) => rest)(userData.file) }] : [] )]
     })
 
     try {
@@ -51,23 +57,34 @@ const generateResponse = async (botMsgDiv) => {
         const responseText = data.candidates[0].content.parts[0].text;
         textElement.textContent = responseText;
         typingEffect(responseText, textElement, botMsgDiv);
+
+        chatHistory.push({ role: "model", parts: [{ text: responseText }] });
+
+        console.log(chatHistory);
     } catch (error) {
         console.log(error);
+    } finally {
+        userData.file = {};
     }
 }
 
 const handleFormSubmit = (e) => {
     e.preventDefault();
-    userMessage = promptInput.value.trim();
+    const userMessage = promptInput.value.trim();
     if (!userMessage) return;
 
     promptInput.value = "";
+    userData.message = userMessage;
 
-    const userMsgHTML = `<p class="message-text"></p>`;
+    const userMsgHTML = `
+        <p class="message-text"></p>
+        ${userData.file.data ? (userData.file.isImage ? `<img src="data:${userData.file.mime_type};base64,${userData.file.data}" class="img-attachment" />` : `<p class="file-attachment"><span class="material-symbols-rounded">description</span></p>`) : ""}
+    `;
+    
     const userMsgDiv = createMsgElement(userMsgHTML, "user-message");
-
     userMsgDiv.querySelector('.message-text').textContent = userMessage;
     chatsContainer.appendChild(userMsgDiv);
+    scrollToBottom();
 
     setTimeout(() => {
         const botMsgHTML = `
@@ -76,8 +93,33 @@ const handleFormSubmit = (e) => {
         `;
         const botMsgDiv = createMsgElement(botMsgHTML, "bot-message", "loading");
         chatsContainer.appendChild(botMsgDiv);
+        scrollToBottom();
         generateResponse(botMsgDiv);
     }, 600);
 }
 
+fileInput.addEventListener('change', () => {
+    const file = fileInput.files[0];
+    if (!file) return;
+
+    const isImage = file.type.startsWith("image/");
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onload = (e) => {
+        fileInput.value = "";
+        const base64String = e.target.result.split(",")[1];
+        fileUploadWrapper.querySelector('.file-preview').src = e.target.result;
+        fileUploadWrapper.classList.add("active", isImage ? "img-attached" : "file-attached");
+
+        userData.file = { fileName: file.name, data: base64String, mime_type: file.type, isImage }
+    }
+})
+
+document.querySelector('#cancle-file-btn').addEventListener('click', () => {
+    userData.file = {};
+    fileUploadWrapper.classList.remove("active", "img-attached", "file-attached");
+})
+
 promptForm.addEventListener('submit', handleFormSubmit);
+promptForm.querySelector('#add-file-btn').addEventListener('click', () => fileInput.click());
